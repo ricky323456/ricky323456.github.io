d80242efa2777cdb2c15a1d69b70b2f614b9147f
Ricky <cobeck22@gmail.com>
2015-12-05 22:23:00 -0600

    Link fix

--- file.js
+++ file.js
@@ -1,6 +1,6 @@
 var correctGuess = false;
 var randomNumber = Math.floor(Math.random() * 6) + 1;
-var guess = prompt("I am thinking of a number betwen 1 and 6. What is it?")
+var guess = prompt("I am thinking of a number between 1 and 6. What is it?")
 if (parseInt(guess) === randomNumber) {
 correctGuess = true;
 }
