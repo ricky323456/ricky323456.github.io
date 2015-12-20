360cfd4ebc556a48efca1668e22d36f2c68d8c4d
Ricky <cobeck22@gmail.com>
2015-12-19 19:21:13 -0600

    Changes in html layout

--- file.js
+++ file.js
@@ -1,1 +1,15 @@
-alert(get.time())
+function getRandomNumber ( upper ){
+	var randomNumber = Math.floor(Math.random() * upper ) + 1;
+	return randomNumber;
+}
+/*
+console.log(getRandomNumber(6) );
+console.log(getRandomNumber(100));
+console.log(getRandomNumber(1000));
+console.log(getRandomNumber(100000));
+*/
+function getArea(width, length, unit){
+	var area = width * length;
+	return area + "" + unit;
+}
+console.log(getArea(10,20,"square feet"));
