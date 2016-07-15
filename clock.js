228bc2e72bcdf1fd31dc9a3660624a81363224be
Ricky <cobeck22@gmail.com>
2016-07-15 00:24:21 -0500

    changed completely

--- clock.js
+++ clock.js
@@ -1,27 +1,3 @@
-function time(){
-	var pcTime = new Date;
-	var hour = pcTime.getHours();
-	var minutes = pcTime.getMinutes();
-	var seconds = pcTime.getSeconds();
-	
-	if (hour > 12){
-		hour -= 12;
+john = function (yo){
+console.log(yo);
 }
-	else if (hour === 0){
-	hour += 12;
-	}
-		
-
-	if(seconds < 10){
-		seconds = "0" + seconds;
-	}
-	if (minutes < 10){
-		minutes = "0" + minutes;
-	}
-
-
-	var call = document.getElementById("clock");
-	call.innerHTML = hour + ":" + minutes + ":" + seconds;
-}
-time();
-setInterval(time, 1000);
