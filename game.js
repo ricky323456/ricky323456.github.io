f51b716e0e76643099d6239565223119db308272
Ricky <cobeck22@gmail.com>
2016-09-10 14:19:35 -0500

    fixed Backgrounds

---
+++ game.js
@@ -0,0 +1,16 @@
+var h=prompt("Give me a number");
+var w=prompt("Give me a number");
+document.getElementById("color").style.height=h+"px";
+document.getElementById("color").style.width=w+"px";
+
+var first =document.getElementById("color");
+
+
+setInterval(function colorchanger(){
+var a=Math.floor(Math.random()*256);
+var b=Math.floor(Math.random()*256);
+var c=Math.floor(Math.random()*256);
+var col ="rgb("+a+","+b+","+c+")";
+first.style.backgroundColor=col;
+}
+,250);
